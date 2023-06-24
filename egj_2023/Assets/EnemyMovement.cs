417aeaf16d0925f705f2a8cc8066ec27ebe87298
Quentin Brechbuehl <quentin.brechbuehl@buildigo.ch>
2023-06-24 13:22:30 +0200

    save

--- egj_2023/Assets/EnemyMovement.cs
+++ egj_2023/Assets/EnemyMovement.cs
@@ -7,14 +7,23 @@ public class EnemyMovement : MonoBehaviour
 {
     // public GameObject player;
     public float speed;
+    public GameObject player;
     public bool canMove = true;
 
     private void Update()
     {
-        var player = GameObject.FindGameObjectWithTag("Player");
+        if(!canMove)return;
+        // var player = GameObject.FindGameObjectWithTag("Player");
         var distance = Vector2.Distance(transform.position, player.transform.position);
         Vector2 direction = player.transform.position - transform.position;
         transform.position =
             Vector2.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("XD");
+        // speed = 0;
+        canMove = false;
     }
 }
