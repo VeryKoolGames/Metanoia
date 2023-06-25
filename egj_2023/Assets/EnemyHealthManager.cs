fefc838907d6d4dd770f154ca4c6f6af53cd886d
Benjamin Vermot <victorvermot@gmail.com>
2023-06-25 13:10:31 +0200

    enemy respawn

---
+++ egj_2023/Assets/EnemyHealthManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemyHealthManager : MonoBehaviour
+{
+    public int health = 2;
+    public GameObject enemyPrefabs;
+    private GameObject player;
+    private GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("EnnemyManager");
+        player = GameObject.FindGameObjectsWithTag("Player")[0];
+        enemy = GameObject.FindGameObjectsWithTag("Enemy")[0];
+    }
+
+    public void Hit(){
+        if(health > 0){
+            health--;
+            Debug.Log("Health is now " + health);
+            if(health != 0){
+                Respawn();
+                return;
+            }
+            StartCoroutine(LevelManager.Instance.LoadLevel(2));
+        }
+    }
+
+    private void Respawn(){
+        Vector3 centerOfRadius = player.transform.position;
+
+        float spawnRadius = Vector3.Distance(player.transform.position, enemy.transform.position);
+        Vector2 randomPoint = Random.insideUnitCircle.normalized * spawnRadius;
+        Vector3 spawnPosition = centerOfRadius + new Vector3(randomPoint.x, randomPoint.y, 10);
+
+        enemy.transform.position = spawnPosition;
+    }
+}
--- egj_2023/Assets/Scripts/BulletColliding.cs
+++ egj_2023/Assets/Scripts/BulletColliding.cs
@@ -9,9 +9,11 @@ public class BulletColliding : MonoBehaviour
     {
         if (col.tag == "Enemy")
         {
-            Destroy(col.gameObject);
+            // Destroy(col.gameObject);
+            GameObject enemy = col.gameObject;
+            enemy.GetComponent<EnnemyHealthManager>().Hit();
             Debug.Log("EXPLOISION");
-            StartCoroutine(LevelManager.Instance.LoadLevel(2));
+            //StartCoroutine(LevelManager.Instance.LoadLevel(2));
         }
     }
 }
